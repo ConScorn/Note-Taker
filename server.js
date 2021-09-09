fe5f4049051218257eb436fc82c2f135ff36b60d
ConScorn <connorscorn@hotmail.com>
2021-09-09 14:34:39 -0500

    finished routes, app is working

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const util = require("util");
+const uniqid = require("uniqid");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -26,7 +27,31 @@ app.get('/notes', (req, res) => {
 
 app.get("/api/notes", (req, res) => res.sendFile(path.join(__dirname, "db/db.json")));
 
+app.post("/api/notes", (req, res) => {
+  let newNote = req.body;
+  newNote.id = uniqid();
+  fs.readFile("db/db.json", "utf8", (err, data) => {
+        if (err) throw err;
+        const parseData = JSON.parse(data);
+        parseData.push(newNote);
+        fs.writeFile("db/db.json", JSON.stringify(parseData), err => {
+            if (err) throw err;
+            res.json(parseData)
+        })
+    })
+})
 
+app.delete("/api/notes/:id", (req, res) => {
+    fs.readFile("./db/db.json", (err, data) => {
+        if (err) throw err;
+        const noteArr = JSON.parse(data).filter((note) => note.id !== req.params.id);
+        fs.writeFile("./db/db.json", JSON.stringify(noteArr), err => {
+            if (err) throw err;
+            res.json(data)
+        })
+    })
+  
+});
 
 
 // start the server
